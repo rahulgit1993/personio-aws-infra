5f4fb93935551caa331ebf061593e324bc35dab5
rahulgit1993 <rahulsregcp@gmail.com>
2025-05-04 15:21:40 +0200

    Commiting changes to new application version

--- app/src/main.go
+++ app/src/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Content-Type", "text/html")
-        fmt.Fprint(w, `<html><body><h1 style="color:blue;">version1</h1></body></html>`)
+        fmt.Fprint(w, `<html><body><h1 style="color:green;">version2</h1></body></html>`)
     })
 
     http.ListenAndServe(":8080", nil)
